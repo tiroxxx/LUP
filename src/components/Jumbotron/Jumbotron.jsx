2eddf3f430d6ff846ed03a94ed9f4c4d52145f67
Christian Marquez <christianmarquez41@gmail.com>
2022-04-26 22:24:04 -0400

    jumbotron heading done

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import JumbotronImg from "../../pictures/web-design-jumbotron.jpg"
 import "./Jumbotron.css"
 
 export default function Jumbotron() {
   return (
     <div className='jumbotron'>
         <div className='jumbotron-heading'>
-
+              <p>Locked</p>
+              <p>Up</p>
+              <p>Potential</p>
+              <h2>Solutions</h2>
         </div>
         </div>
   )
