e4a48d1d89575e4b2380e4c0577c1c5ab42dd5d2
Christian Marquez <christianmarquez41@gmail.com>
2022-05-08 21:44:28 -0400

    more styling for smaller screens

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -5,9 +5,9 @@ export default function Jumbotron() {
   return (
     <div className='jumbotron'>
         <div className='jumbotron-heading'>
-              <p>Locked</p>
-              <p>Up</p>
-              <p>Potential</p>
+              <h1>Locked</h1>
+              <h1>Up</h1>
+              <h1>Potential</h1>
               <h2>Solutions</h2>
         </div>
         </div>
